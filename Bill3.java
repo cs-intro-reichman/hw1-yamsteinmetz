768a9be545a5dccda6c37b00ba66cd1b89790a7f
yamsteinmetz <yam.steinmetz@post.runi.ac.il>
2024-11-07 09:41:19 +0200

    yam homework1 bill3

--- Bill3.java
+++ Bill3.java
@@ -1,8 +1,24 @@
-// Splits a restaurant bill evenly among three diners.
 public class Bill3 {
-	public static void main(String[] args) {
-		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
-	}
+
+    public static void main(String[] args) {
+
+        // create 3 variable of names 
+        String name1 = args[0];
+        String name2 = args[1];
+        String name3 = args[2];
+
+            // create variable of total bill and Calculate personal bill
+            int totalBill;
+            totalBill = Integer.parseInt(args[3]); 
+            double personalBill;
+            personalBill = totalBill/ 3.0 ;
+            
+                // round the number up
+                personalBill = Math.ceil(personalBill);
+                    //print the data with the output
+                    System.out.println("Dear " + name1+ ", " + name2 + " ,and " + name3+ " pay " + personalBill + " Shekels each");
+     
+
+    }
+   
 }
